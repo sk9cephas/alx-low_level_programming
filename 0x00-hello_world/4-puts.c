f2cf1df5d4fc2f72deb543d310902b9b00a44e36
sk9cephas <skcephas123@gmail.com>
2022-10-18 13:23:20 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+
+	
+{
+puts("\" progamming is like building a multilingual puzzle)
+		return (0);
+}
