a57ffd1e19b5b7db64f102a4ae507d4f7e2b2158
sk9cephas <skcephas123@gmail.com>
2022-10-25 03:41:01 -0700

    return the lenght of string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,22 +5,15 @@
  * @s: string
  * Return: lenght
  */
-<<<<<<< HEAD
-
-=======
->>>>>>> f031bd673b749efeee9555cd78f131fa920fee8b
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    while (*s != '\0')
-    {
-        len++;
-        s++;
-    }
-    return (len);
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> f031bd673b749efeee9555cd78f131fa920fee8b
