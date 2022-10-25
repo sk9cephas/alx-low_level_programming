1628e380a231dc9be18e71d5881d96e64be58035
sk9cephas <skcephas123@gmail.com>
2022-10-25 03:33:42 -0700

    function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * 1-sawp - swap two integer
+ * @a: first pointer
+ * @b:second pointer
+ * Return: void
+ */
+
+void swap_int(int*a, int *b)
+{
+	*a = 98;
+	
+	*b = 66;
+
+	*
