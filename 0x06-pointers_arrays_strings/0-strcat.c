419bd950ca4138eac66e7aefa4eeea303f611278
sk9cephas <114515167+sk9cephas@users.noreply.github.com>
2022-10-26 21:27:06 +0000

    0-strcat trailing sapces checked1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,10 +11,11 @@
 char *_strcat(char *dest, const char *src)
 {
 	int index = 0, dest_len = 0;
+	
 	while (dest[index++])
-		dest_len++;
+	dest_len++;
 	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
+	dest[dest_len++] = src[index];
+	
 	return (dest);
 }
