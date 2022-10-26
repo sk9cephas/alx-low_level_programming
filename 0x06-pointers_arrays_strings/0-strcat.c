b94b01569255eed88525dac474570afaa15fe57e
sk9cephas <skcephas123@gmail.com>
2022-10-26 14:43:38 -0700

    _putcha update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,14 +8,3 @@
  * Return: A pointer to the destination string @dest.                                                                                
  */
 
-char *_strcat(char *dest, const char *src)
-{
-	int index = 0, dest_len = 0;
-	
-	while (dest[index++])
-	dest_len++;
-	for (index = 0; src[index]; index++)
-	dest[dest_len++] = src[index];
-	
-	return (dest);
-}
