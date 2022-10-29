75e72fb23b19c0a6deadfe9d74d3c1d56566398f
sk9cephas <sk.cephas.0852@gmail.com>
2022-10-29 06:49:00 -0700

    commmit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry
+ * Return: 1
+ */
+int main(void)
+{
+	write(1, "and that pieceof art iis useful\" - Dora Korpar, 2015-10-19\n",59):
+		rturn (1)
+}
