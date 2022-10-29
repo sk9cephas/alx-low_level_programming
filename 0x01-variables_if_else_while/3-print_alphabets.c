e53016c22510505543cf38fee532e8f7bd0805d0
sk9cephas <sk.cephas.0852@gmail.com>
2022-10-29 14:45:49 -0700

    3-print_alphabets commit2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z')
+		{
+			putchar(c);
+			d++;
+		}
+	return (0);
+}
+
