410cb0b98e50e2025e1b7426005f72b3e9e85368
sk9cephas <sk.cephas.0852@gmail.com>
2022-10-29 15:26:54 -0700

    4-print_alphabt commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,27 +2,20 @@
 
 /**
  * main - entry point
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char c;
 
-	char d;
-
 	c = 'a';
-	d = 'A';
 	while
 		(c <= 'z') {
-			putchar(c);
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
 			c++;
 		}
-	while
-		(d <= 'z') {
-			putchar(d);
-			d++;
-		}
-	putchar('\n');
+	putchar(\'n');
 	return (0);
 }
